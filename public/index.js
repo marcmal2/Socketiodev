839d065309b41053783df2695c60b0bcb896e997
mmald <mmaldonadojr@hotmail.com>
2022-12-13 22:28:47 -0600

    multiple players can see eachother and move

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 const mapImage = new Image();
 mapImage.src = "/Tilled Dirt.png";
 
+const sprite = new Image();
+sprite.src = "/Owlet_Monster.png";
+
 const canvasEl = document.getElementById("canvas");
 canvasEl.width = window.innerWidth;
 canvasEl.height = window.innerHeight;
@@ -9,6 +12,7 @@ const canvas = canvasEl.getContext("2d");
 const socket = io(`ws://localhost:5000`);
 
 let map = [[]];
+let players = [];
 
 const TILE_SIZE = 16;
 
@@ -18,6 +22,43 @@ socket.on("connect", () => {
 
 socket.on("map", (loadedMap) => {
   map = loadedMap;
+});
+
+socket.on("players", (serverPlayers) => {
+  players = serverPlayers;
+});
+
+const inputs = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+};
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "w") {
+    inputs["up"] = true;
+  } else if (e.key === "s") {
+    inputs["down"] = true;
+  } else if (e.key === "d") {
+    inputs["right"] = true;
+  } else if (e.key === "a") {
+    inputs["left"] = true;
+  }
+  socket.emit("inputs", inputs);
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.key === "w") {
+    inputs["up"] = false;
+  } else if (e.key === "s") {
+    inputs["down"] = false;
+  } else if (e.key === "d") {
+    inputs["right"] = false;
+  } else if (e.key === "a") {
+    inputs["left"] = false;
+  }
+  socket.emit("inputs", inputs);
 });
 
 function loop() {
@@ -43,6 +84,11 @@ function loop() {
       );
     }
   }
+ 
+  for (const player of players) {
+    canvas.drawImage(sprite, player.x, player.y);
+  }
+
 
   window.requestAnimationFrame(loop);
 }
